7b6896143298b259524b9c417e01b4b49c777aff
ravi2krishna <ravi2krishna@gmail.com>
2024-11-15 09:36:18 +0530

    update vars for vpc

--- vpc-setup/variables.tf
+++ vpc-setup/variables.tf
@@ -7,3 +7,30 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
   type = string
 }
+
+# Variable VPC CIDR
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+# Variable VPC Tenancy
+variable "vpc_tenancy" {
+  type = string
+  default = "default"
+}
+
+# Variable VPC Name
+variable "vpc_name" {
+  type = string
+  default = "login"
+}
+
+# Variable Public Subnets
+variable "public_subnet_cidrs" {
+  type = map(string)
+  default = {
+    frontend = "10.0.0.0/24"
+    backend = "10.0.1.0/24" 
+  }
+}
